8f41546b620343c20b782d5d6efd2cedd9a6285a
popcivilar <zcwdgz100700@163.com>
2019-11-10 20:34:58 +0800

    初始化程序

---
+++ src/main/java/com/popcivilar/code/FakeHashMap.java
@@ -0,0 +1,150 @@
+package com.popcivilar.code;
+
+/**
+ * 手写HashMap
+ */
+public class FakeHashMap<K,V> {
+
+    private volatile Entry<K,V>[] table;
+
+    private volatile int entrySize; // 数组长度
+
+    private static volatile int initSize = 16;
+
+
+    public FakeHashMap() {
+        this.table = new Entry[initSize];
+        this.entrySize = table.length;
+    }
+
+    static class Entry<K,V>{
+        private Node<K,V> node;
+
+
+    }
+
+
+    static class Node<K,V>{
+
+        private K key;
+
+        private V val;
+
+        private Node next;
+
+        public Node(K k, V v) {
+            this.key = k;
+            this.val = v;
+        }
+    }
+
+
+    public V put(K k,V v){
+        V oldVal = null;
+        if(k == null){
+            return putEmptyKey(v);
+        }
+        int hash = k.hashCode();
+        int index = hash % entrySize;
+        if(table[index] != null){
+            Entry<K,V> entry = table[index];
+            Node<K, V> node = entry.node;
+            if(node != null){
+                Node<K,V> tail = null;
+                for(Node<K,V> p = node;p != null ; p = p.next){
+                    if(p.key.hashCode() == hash && p.key.equals(k)){
+                       oldVal = p.val;
+                       p.val = v;
+                       return oldVal;
+                    }
+                    tail = p;
+                }
+                if(tail != null){
+                    tail.next = new Node(k,v);
+                }
+
+            }else{
+                putNode(entry,node,k,v);
+                oldVal = v;
+            }
+
+        }else{
+            //entry == null
+            Entry<K, V> newEntry = new Entry<>();
+            Node node = new Node(k, v);
+            newEntry.node = node;
+            table[index] = newEntry;
+            oldVal = v;
+        }
+        //todo 考虑扩容
+
+        return oldVal;
+    }
+
+    private void putNode(Entry<K,V> entry,Node<K,V> node, K k, V v) {
+        Node<K,V> newNode = new Node<>(k,v);
+        if(node != null){
+            node.next = newNode;
+        }else{
+            entry.node = newNode;
+        }
+    }
+
+
+    int size(){
+       int size = 0;
+       for(int i = 0;i<table.length;i++){
+           if(table[i] != null){
+               for(Node<K,V> node = table[i].node;node!=null;node = node.next){
+                   size++;
+               }
+           }
+       }
+       return size;
+    }
+
+
+    public V get(K k){
+        if(k == null){
+            return getEmptyKey(k);
+        }
+        int hash = k.hashCode();
+        int index = hash % entrySize;
+        if(table[index] != null){
+            Entry<K,V> entry = table[index];
+            Node<K, V> node = entry.node;
+            for(Node<K,V> p = node;p != null;p = p.next){
+                if(k.hashCode() == hash && k.equals(p.key)){
+                    return p.val;
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 获取空值
+     * @param k
+     * @return
+     */
+    private V getEmptyKey(K k) {
+
+        return  null;
+    }
+
+
+    /**
+     * 存放空值
+     * @param v
+     * @return
+     */
+    private V putEmptyKey(V v) {
+
+        //todo  键 空 存储策略
+
+        return null;
+
+    }
+
+}
